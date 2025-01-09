ae8c4d3143d39459ec05c36a5a153e4be18ab35d
Trollguyjr <90011577+Trollguyjr@users.noreply.github.com>
2025-01-08 23:39:36 -0800

    swerve

--- Code_2025/src/main/java/frc/robot/subsystems/SwerveMod.java
+++ Code_2025/src/main/java/frc/robot/subsystems/SwerveMod.java
@@ -1,5 +1,11 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Rotation;
+
+import java.util.concurrent.CancellationException;
+
+import org.opencv.core.RotatedRect;
+
 import com.ctre.phoenix.sensors.AbsoluteSensorRange;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
@@ -9,9 +15,13 @@ import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.Constants.ModConstants;
 
@@ -19,49 +29,93 @@ public class SwerveMod {
     private final TalonFX DriveMotor, SteerMotor;
     private final CANcoder SteerEncoder;
 
-    private final SlewRateLimiter drivelim = new SlewRateLimiter(1);
+    // private final SlewRateLimiter drivelim = new SlewRateLimiter(1);
 
     // Incase Slew Rate Limiter doesn't work for drive
-    // private final PIDController drivecont = new PIDController(ModConstants.KP,0,0);
+    private final PIDController driveCont = new PIDController(ModConstants.KP,0,0);
     
     private final PIDController steerContrl = new PIDController(ModConstants.KP, 0, 0);
 
     private final String moduleName;
 
-    private final SwerveModuleState lastDesiredState = new SwerveModuleState();
+    private  SwerveModuleState lastDesiredState = new SwerveModuleState();
 
 
-    public SwerveMod(int DriveM_ID, int SteerM_ID, int encoderID, double encoderOffset, String modName){
+    public SwerveMod(int DriveM_ID, int SteerM_ID, int encoderID, double encoderOffset, boolean inverted,String modName){
         // Bring the empty variables in here to create objects using the IDs required above
         TalonFXConfiguration talonConfig = new TalonFXConfiguration();
-
-        // TalonFXConfigurator talonConfigurator = DriveMotor.getConfigurator();
-
-        var Motorconfigs = new MotorOutputConfigs();
-        Motorconfigs.Inverted = InvertedValue.Clockwise_Positive;
-        
-        // talonConfigurator.refresh(talonConfig);
-
+        if(inverted){
+            talonConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        }
+        else{
+            talonConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        }
         
         DriveMotor = new TalonFX(DriveM_ID);
         SteerMotor = new TalonFX(SteerM_ID);
+
+        DriveMotor.getConfigurator().apply(talonConfig);
+        SteerMotor.getConfigurator().apply(new TalonFXConfiguration());
+
+        DriveMotor.setNeutralMode(NeutralModeValue.Brake);
+        SteerMotor.setNeutralMode(NeutralModeValue.Brake);
+
         SteerEncoder = new CANcoder(encoderID);
         
+
         CANcoderConfiguration config = new CANcoderConfiguration();
         config.MagnetSensor.MagnetOffset = encoderOffset;
+        config.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1;
+        config.MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive;
+
+        SteerEncoder.getConfigurator().apply(config);
+    
+
+        steerContrl.enableContinuousInput(-Math.PI, Math.PI);
+        driveCont.enableContinuousInput(-Math.PI, Math.PI);
 
 
-
+        steerContrl.reset();
+        driveCont.reset();
 
         moduleName = modName;
 
-        
+    }
 
+    public Rotation2d getRotation(){
+        return Rotation2d.fromRotations(SteerEncoder.getAbsolutePosition().getValueAsDouble());
+    }
+    public double getDrivePos(){
+        return DriveMotor.getPosition().getValueAsDouble();
+    }
+    public double getDriveVelo(){
+        return DriveMotor.getVelocity().getValueAsDouble();
+    }
+    public SwerveModuleState getModState(){
+        return new SwerveModuleState(getDriveVelo(),getRotation());
+    }
+    public SwerveModulePosition getModPos(){
+        return new SwerveModulePosition(getDrivePos(),getRotation());
     }
 
 
+    public void setModState(SwerveModuleState desiredState){
+        SwerveModuleState optimizedState = new SwerveModuleState(desiredState.speedMetersPerSecond,getRotation());
+       
+        if(Math.abs(lastDesiredState.speedMetersPerSecond) < 0.02){
+            stop();
+            return;
+        }
 
-    public void setModState(){
+        //Might need massive change
+        DriveMotor.set(driveCont.calculate(optimizedState.speedMetersPerSecond * 8, lastDesiredState.speedMetersPerSecond));
+        SteerMotor.set(steerContrl.calculate(getRotation().getRadians(), optimizedState.angle.getRadians()));
 
+        lastDesiredState = desiredState;
+    }
+
+    public void stop(){
+        DriveMotor.set(0);
+        SteerMotor.set(0);
     }
 }
