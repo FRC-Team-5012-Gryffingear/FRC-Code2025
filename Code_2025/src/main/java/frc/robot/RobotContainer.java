daa48faaa7bf37b4eba7f01f7bc09a3702710fd0
Trollguyjr <agonzalez3411@tpaa.org>
2025-01-21 16:28:58 -0800

    PathPlanningStart

--- Code_2025/src/main/java/frc/robot/RobotContainer.java
+++ Code_2025/src/main/java/frc/robot/RobotContainer.java
@@ -12,8 +12,19 @@ import frc.robot.commands.SwerveCom;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SwerveSubsys;
 import frc.robot.subsystems.limeyImproved;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.events.EventTrigger;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -27,7 +38,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   // private final limey lime = new limey();
-
+  private final SendableChooser<Command> autoChooser;
   private final SwerveSubsys swerve = new SwerveSubsys();
 
   private final limeyImproved limeI = new limeyImproved();
@@ -45,6 +56,15 @@ public class RobotContainer {
      swerve.setDefaultCommand(new SwerveCom(
       swerve, 
       driverController));
+    
+    // Build an auto chooser. This will use Commands.none() as the default option.
+    autoChooser = AutoBuilder.buildAutoChooser("Test Auto");
+    NamedCommands.registerCommand("WORKING", Commands.print("AUTO IS WORKING"));
+    autoChooser.addOption("MOVE", new PathPlannerAuto("Test Auto"));
+    // Another option that allows you to specify the default auto by its name
+    // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+
+    SmartDashboard.putData("Auto Chooser", autoChooser);
 
     configureBindings();
   }
@@ -75,6 +95,16 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return null;// Autos.exampleAuto(m_exampleSubsystem);
+    try{
+        // Load the path you want to follow using its name in the GUI
+        PathPlannerPath path = PathPlannerPath.fromPathFile("TestPath");
+
+        // Create a path following command using AutoBuilder. This will also trigger event markers.
+        return AutoBuilder.followPath(path);
+    } catch (Exception e) {
+        DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
+        return Commands.none();
+    }
+    // return autoChooser.getSelected();// Autos.exampleAuto(m_exampleSubsystem);
   }
 }
